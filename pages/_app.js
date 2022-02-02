f652a4159e36f0e68e0161f7621c01af951358b5
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-02 17:51:26 +0900

    [Next] Fetch

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,11 @@
 import Layout from "../components/Layout";
 
 import Head from "next/head";
+import NavBar from "../components/NavBar";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Head></Head>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-          text-decoration: none;
-        }
-        h1 {
-          color: red;
-        }
-        nav {
-          background-color: transparent;
-        }
-
-        .active {
-          background: green;
-        }
-      `}</style>
     </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "7d18deb10f51bdfc283dca027e840341";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
-      <Seo title="Home"></Seo>
-      <h1>Hello</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading....</h4>}
+      {movies?.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
