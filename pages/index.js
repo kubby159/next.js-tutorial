e01053a6e2b73d25be28173260df98e038610bc0
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-03 11:59:18 +0900

    [Next] ServerSideRendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +35,18 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// getServerSideProps
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    // 서버사이드를 통해 해당 props는 page에게 전달됨.
+    props: {
+      results,
+    },
+  };
 }
